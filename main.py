1b4e4dd583ae40174b98c01d3ed8c74f93db6ab4
Hilal Kurt <hilal.kurt304@gmail.com>
2024-02-27 19:47:57 +0300

    turtle crossing capstone game improved version (turtle hearts)

--- main.py
+++ main.py
@@ -6,52 +6,72 @@ Created on Thu Dec 14 17:18:47 2023
 """
 
 from turtle import Screen;
+import turtle
 from obstacles import Obstacles, cars;
 
 import time
-Turtles = [];
+
 
 class turtle_crossing_capstone_game():
     def __init__(self):
+        
         self.screen = Screen();
+        self.screen.bgcolor("papaya whip")
         self.screen.tracer(0)
         self.screen.screensize(600,600);
         self.screen.setworldcoordinates(-300, -300, 300, 300);
         self.obstacles = Obstacles();
         self.screen.listen();
-        # create cars with random coordinates
-        for i in range(50):
-            self.cars = cars();
-            Turtles.append(self.cars);
+        turtle.title("Turtle Crossing Game")
+    # create cars with random coordinates
+        self.cars = cars();
+        self.Turtles = self.cars.Turtles; # create cars on the screen at the beginning
             
         self.screen.onkeypress(fun= self.obstacles.move_turtle_up, key="Up");
         self.game_on(True);
     
     def game_on(self,condition):
-        
+        self.new_car_counter = 0; # counter for adding new cars to the game
+        self.turtle_heart = 3;
         self.crash = False;
-        self.obstacles.level_game();
+        self.obstacles.level_game(); # write current level on the screen
         
         while condition:
-            for i in Turtles:
+            for i in self.Turtles: # move cars forward
                 self.cars.move_car(i);
-            
-            for i in Turtles:
-               # check whether car crash with the turtle or not
+
+            for i in self.Turtles:
+                # check whether car crash with the turtle or not
                 if i.xcor() <= 15 and i.xcor() >= -15:
-                    if i.distance(self.obstacles.turtle) <= 23:
+                    if i.distance(self.obstacles.turtle) <= 20:
                         self.crash = True;
                         break;
-            if self.crash == True: # if there is a crash, finish the game
-                self.obstacles.game_over();
-                break;
+
+            if self.crash == True: # if there is a crash, decrease the turtle heart. If turtle heart is zero, finish the game
+                self.turtle_heart -= 1;
+                
+                self.obstacles.turtle_hearts(self.turtle_heart); # decrease the turtle heart on the screen
+                
+                if self.turtle_heart < 0: # if heart of turtle is finished, finish the game  
+                    self.obstacles.game_over();
+                    break
+                else:
+                    self.crash = False; 
+      
             # if turtle reaches the upper wall increase the level
             if self.obstacles.turtle.ycor() >= 300:
-                self.obstacles.next_level();
-                self.obstacles.new_level(Turtles);
-                
-            self.screen.update();
-            time.sleep(self.obstacles.speed)
+                self.obstacles.next_level(); # increase speed of the cars and level
+   
+            
+            if self.new_car_counter % 3 == 0: # create new cars with new positions 
+                self.cars.new_cars();   
+            self.new_car_counter += 1;
+            
+            
+            
+            self.screen.update(); # update screen
+            time.sleep(self.obstacles.speed) 
+            self.Turtles = self.cars.delete_car(self.Turtles);
         self.screen.exitonclick();
     
 
--- obstacles.py
+++ obstacles.py
@@ -5,31 +5,59 @@ Created on Thu Dec 14 17:25:25 2023
 @author: Hilal
 """
 
-from turtle import Turtle;
+from turtle import Turtle,Screen;
+
 import random
 
-obstacle_colors = ["medium aquamarine", "hot pink", "pale violet red", "gold", "light blue", "light steel blue", "powder blue", "light sky blue", "pale green"];
+obstacle_colors = ["firebrick", "deep pink", "dark green", "teal", "dark blue", "steel blue", "sea green", "maroon", "dark orchid"];
 
 
-class cars(Turtle): # create car blueprint to create car objects with random coordinates
+class cars(): # create cars with random coordinates
     def __init__(self):
-        super().__init__();
-        self.penup()
-        self.color(random.choice(obstacle_colors));
-        self.shape("square");
-        self.shapesize(1,2)
-        self.hideturtle();
-        self.setposition(random.randint(300,2500), random.randint(-250,250))
-        self.showturtle();
-        self.setheading(180);
+        self.Turtles = [];
+        for i in range(55): # create car at the beginning of the game
+            self.Turtles.append(self.create_car(-300,700));
+        
+        
+    def create_car(self,beginning_position,end_position): # create new car
+        self.turtle = Turtle();
+        self.turtle.penup()
+        self.turtle.color(random.choice(obstacle_colors))
+        self.turtle.shape("square")
+        self.turtle.shapesize(1,2)
+        self.turtle.hideturtle();
+        self.turtle.setposition(random.randint(beginning_position,end_position), random.randint(-230,230))
+        self.turtle.showturtle();
+        self.turtle.setheading(180);
+        return self.turtle
+    
+    def new_cars(self): # create car during the game
+        self.Turtles.append(self.create_car(300,350));
+            
+    def total_cars(self): # return total cars
+        return self.Turtles
 
-    def move_car(self, i):
-        i.forward(10);
+    def move_car(self, i): # move cars forward
+        i.forward(5);
+    
+    def delete_car(self,turtles): # delete cars which are out of game
+
+        for i in turtles:
+            x,y = i.position();
+            if x<-300:
+                i.hideturtle();
+                turtles.remove(i);
+
+        return turtles
 
 
 class Obstacles():
     
     def __init__(self):
+        
+        image = "turtle_heart.gif";
+        screen = Screen();
+        screen.addshape(image);
         self.turtle = Turtle(); # create turtle object 
         self.turtle.penup();
         self.turtle.shape("turtle");
@@ -38,32 +66,77 @@ class Obstacles():
         self.turtle.setpos(0,-290);
         self.turtle.color("dark green");
         
+        # create writer turtle
         self.turtle2 = Turtle();
         self.turtle2.hideturtle();
         self.turtle2.penup();
         self.turtle2.color("black");
         self.level = 1;
         self.speed = 0.1;
+       
+        # turtle heart1
+        self.turtle_h1 = Turtle(); # create turtle object 
+        self.turtle_h1.penup();
+        self.turtle_h1.shape(image);
+        self.turtle_h1.setheading(90);
+        self.turtle_h1.setpos(210,280);
+        
+        # turtle heart2
+        self.turtle_h2 = Turtle(); # create turtle object 
+        self.turtle_h2.penup();
+        self.turtle_h2.shape(image);
+        self.turtle_h2.setheading(90);
+        self.turtle_h2.setpos(240,280);
+        
+        # turtle heart3
+        self.turtle_h3 = Turtle(); # create turtle object 
+        self.turtle_h3.penup();
+        self.turtle_h3.shape(image);
+        self.turtle_h3.setheading(90);
+        self.turtle_h3.setpos(270,280);
+
     
-    def move_turtle_up(self):
+    def move_turtle_up(self): # move forward
         self.turtle.forward(10)
         
-    def game_over(self):
+    def game_over(self): # inform user when game is over.
+
         self.turtle2.setpos(-10,0);
         self.turtle2.write("GAME  OVER", align = "center",  font=('Arial', 20, 'bold'));
+        return True
     
     def next_level(self): # increase level and increase the speed
         self.level += 1;
-        self.speed -= 0.01;
+        self.speed -= 0.015;
+        self.turtle.setpos(0,-290); # set turtle position at the beginning
         self.level_game();
         
-    def level_game(self):   
+    def level_game(self):   # write current level of the game
         self.turtle2.clear();
         self.turtle2.setpos(-260,270);
         self.turtle2.write("LEVEL: {0}".format(self.level), align = "center",  font=('Arial', 13, 'bold'));
     
-    def new_level(self, Turtles): # set turtle position randomly for new level 
-        self.turtle.setpos(0,-290);
-        for i in Turtles:
-            i.setposition(random.randint(300,2500), random.randint(-250,250))
+    def turtle_hearts(self,turtle_heart): # decrease the turtle heart and check whether the game is finished or not.
 
+        if turtle_heart == 1:
+            self.turtle_h2.hideturtle();
+            self.turtle.setpos(0,-290);
+
+        
+       
+        elif turtle_heart == 2:
+            self.turtle_h1.hideturtle();
+            self.turtle.setpos(0,-290);
+
+            
+        else:
+            self.turtle_h3.hideturtle();
+           
+            
+
+   
+
+            
+      
+       
+        
